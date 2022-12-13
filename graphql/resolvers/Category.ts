861e49690333654b3341b2689213288f6a77dbee
Ryan Yang <ryanyang.nz@gmail.com>
2022-12-13 17:52:57 +1300

    update

--- graphql/resolvers/Category.ts
+++ graphql/resolvers/Category.ts
@@ -1,14 +1,44 @@
 export const Category = {
-  products: (parent, { filter }, { products }) => {
-    const categoryId = parent.id;
-    if (!filter) return products;
-    const { onSale } = filter;
-    if (onSale) {
-      return products.filter(
+  products: (parent, { filter }, { products, reviews }) => {
+    const { onSale, avgRating } = filter;
+
+    if (onSale && avgRating) {
+      const filteredProducts = products.filter(
         (product) =>
           product.onSale === onSale && product.categoryId === parent.id
       );
-    } else
-      return products.filter((product) => product.categoryId === categoryId);
+      const filteredProducts2 = filteredProducts.filter((product) => {
+        const productReviews = reviews.filter(
+          (review) => review.productId === product.id
+        );
+        const productAvgRating =
+          productReviews.reduce((acc, review) => acc + review.rating, 0) /
+          productReviews.length;
+        return productAvgRating >= avgRating;
+      });
+      return filteredProducts2;
+    } else if (onSale) {
+      const filteredProducts = products.filter(
+        (product) =>
+          product.onSale === onSale && product.categoryId === parent.id
+      );
+      return filteredProducts;
+    } else if (avgRating) {
+      const filteredProducts = products.filter(
+        (product) => product.categoryId === parent.id
+      );
+      const filteredProducts2 = filteredProducts.filter((product) => {
+        const productReviews = reviews.filter(
+          (review) => review.productId === product.id
+        );
+        const productAvgRating =
+          productReviews.reduce((acc, review) => acc + review.rating, 0) /
+          productReviews.length;
+        return productAvgRating >= avgRating;
+      });
+      return filteredProducts2;
+    } else {
+      return products.filter((product) => product.categoryId === parent.id);
+    }
   },
 };
