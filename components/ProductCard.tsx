74a7e813d27b464ec8c960b51f369e5057195b59
Ryan Yang <ryanyang.nz@gmail.com>
2022-12-22 12:22:42 +1300

    build new ProductCard

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,45 +1,119 @@
 import React from "react";
+import styled from "styled-components";
+import { BiPlus, BiMinus } from "react-icons/bi";
+
+const CardWrapper = styled.div`
+  border: 2px #000000 solid;
+  max-width: 20rem;
+  border-radius: 0.3rem;
+`;
+
+const CardGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+`;
+
+const Image = styled.div`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  padding: 2rem;
+  border-bottom: 2px #000 solid;
+`;
+
+const Content = styled.div`
+  padding: 2rem 2rem 1.2rem 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 0.5rem;
+  flex: 1 0 0;
+`;
+
+const ProductName = styled.div`
+  font-size: 1.2rem;
+  font-weight: 600;
+`;
+
+const ProductDescription = styled.div`
+  font-size: 1rem;
+  font-weight: 400;
+`;
+
+const ProductAddToCart = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 1rem;
+  font-weight: 400;
+  width: 100%;
+  margin-top: 0.8rem;
+`;
+
+const Price = styled.div`
+  font-size: 1.5rem;
+
+  font-family: ${({ theme }) => theme.fonts.monument};
+
+  span {
+    font-size: 0.8rem;
+    // the span move up a little bit
+    position: relative;
+    top: -0.2rem;
+    margin-right: 0.2rem;
+  }
+`;
+
+const AddToCartButtonGroup = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 0.2rem 0.5rem;
+  border: 2px #000 solid;
+  border-radius: 0.3rem;
+  font-size: 1.1rem;
+  font-weight: 600;
+  width: 7rem;
+  cursor: pointer;
+
+  // select the second div
+  input {
+    font-size: 1.3rem;
+    cursor: auto;
+    width: 2rem;
+    text-align: center;
+  }
+`;
 
 export default function Card() {
   return (
-    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-      <a href="#">
-        <img
-          className="rounded-t-lg"
-          src="/docs/images/blog/image-1.jpg"
-          alt=""
-        />
-      </a>
-      <div className="p-5">
-        <a href="#">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
-          </h5>
-        </a>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
-        </p>
-        <a
-          href="#"
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Read more
-          <svg
-            aria-hidden="true"
-            className="w-4 h-4 ml-2 -mr-1"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-        </a>
-      </div>
-    </div>
+    <>
+      <CardWrapper>
+        <CardGrid>
+          <Image>
+            <img src="/hero-image.png" alt="" />
+          </Image>
+          <Content>
+            <ProductName>Strawberry Banana Chocolate</ProductName>
+            <ProductDescription>
+              Filled with half strawberry cream and jam, half banana cream and
+              chocolate Ganache.
+            </ProductDescription>
+            <ProductAddToCart>
+              <Price>
+                <span>$</span>4.5
+              </Price>
+              <AddToCartButtonGroup>
+                <BiMinus />
+                <input defaultValue={2} />
+                <BiPlus />
+              </AddToCartButtonGroup>
+            </ProductAddToCart>
+          </Content>
+        </CardGrid>
+      </CardWrapper>
+    </>
   );
 }
