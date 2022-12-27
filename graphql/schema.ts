e9e87c99cdc22546b3ed1e89fc2d9eaad0bec4a9
Ryan Yang <ryanyang.nz@gmail.com>
2022-12-27 16:21:21 +1300

    add interface

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,47 +1,4 @@
 import { gql } from "apollo-server-micro";
-import { makeSchema } from "nexus";
-import { objectType, queryType } from "nexus";
-import { join } from "path";
-
-// export const typeDefs = gql`
-//   type Query {
-//     hello: String
-//     products: [Product!]!
-//     categories: [Category!]!
-//   }
-
-//   type Mutation {
-//     createProduct(input: AddProductInput!): Product!
-//     updateProduct(id: ID!, input: AddProductInput!): Product!
-//     removeProduct(id: ID!): Product!
-//   }
-
-//   input AddProductInput {
-//     name: String!
-//     price: Float!
-//     imageUrl: String!
-//     description: String!
-//   }
-
-//   type Product {
-//     id: ID!
-//     name: String!
-//     price: Float!
-//     imageUrl: String!
-//     description: String!
-//     category: Category!
-//     createdAt: String!
-//     updatedAt: String!
-//   }
-
-//   type Category {
-//     id: ID!
-//     name: String!
-//     products: [Product!]!
-//     createdAt: String!
-//     updatedAt: String!
-//   }
-// `;
 
 export const typeDefs = gql`
   type Query {
---
+++ prisma/type.ts
@@ -0,0 +1,18 @@
+export interface IProduct {
+  id: string;
+  name: string;
+  price: number;
+  imageUrl: string;
+  description: string;
+  category: ICategory;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface ICategory {
+  id: string;
+  name: string;
+  products: IProduct[];
+  createdAt: string;
+  updatedAt: string;
+}
